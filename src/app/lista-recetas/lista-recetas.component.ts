97c9dba6307781926f723e15b8d7091b7ec4290b
Leonelson Palima <leonelsonpalima@gmail.com>
2018-09-29 20:53:45 -0500

    Se agrega el componente listaRecetas

---
+++ src/app/lista-recetas/lista-recetas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-recetas',
+  templateUrl: './lista-recetas.component.html',
+  styleUrls: ['./lista-recetas.component.css']
+})
+export class ListaRecetasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
